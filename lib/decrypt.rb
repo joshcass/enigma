b289aa30e2aefc6b51dd400446cefd4185f61571
Josh Cass <joshuacass@gmail.com>
2015-04-14 21:36:16 -0600

    now with file overwrite protection

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,5 +1,6 @@
 require_relative 'enigma'
 
+
 handle = File.open("#{ARGV[0]}", "r")
 message = handle.read.chomp
 
@@ -17,8 +18,18 @@ end
 
 result = Enigma.new(message, key, date).decrypt
 
-writer = File.open("#{ARGV[1]}", "w")
-writer.write(result + "\n")
-writer.close
+if File::exists?("#{ARGV[1]}")
+  puts "***Warning***\nThis file already exists, would you like to overwrite it?\n'y' to overwrite, any other key to cancel"
+  answer = $stdin.gets.chomp
+  if answer == "y"
+    @writer = File.open("#{ARGV[1]}", "w")
+  else
+    abort("Operation Canceled")
+  end
+else
+  @writer = File.open("#{ARGV[1]}", "w")
+end
+@writer.write(result + "\n")
+@writer.close
 
 puts "Created 'decrypted.txt' with the key #{key} and date #{date}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -10,9 +10,19 @@ date = Offset.today
 
 result = Enigma.new(message, key, date).encrypt
 
-writer = File.open("#{ARGV[1]}", "w")
-writer.write(result + "\n")
-writer.close
+if File::exists?("#{ARGV[1]}")
+  puts "***Warning***\nThis file already exists, would you like to overwrite it?\n'y' to overwrite, any other key to cancel"
+  answer = $stdin.gets.chomp
+  if answer == "y"
+    @writer = File.open("#{ARGV[1]}", "w")
+  else
+    abort("Operation Canceled")
+  end
+else
+  @writer = File.open("#{ARGV[1]}", "w")
+end
+@writer.write(result + "\n")
+@writer.close
 
 puts "Created 'encrypted.txt' with the key #{key} and date #{date}"
 
